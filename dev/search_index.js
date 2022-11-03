26655ab200dd7060a6e79636949b40e744903be4
Documenter.jl <documenter@juliadocs.github.io>
2022-11-03 06:33:53 +0000

    build based on adec0f2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EventStudies","category":"page"},{"location":"#EventStudies","page":"Home","title":"EventStudies","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EventStudies.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EventStudies]","category":"page"}]
+}
