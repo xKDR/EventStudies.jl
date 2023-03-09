eb61bfd339e1fb95d38bbd805f29015fb47427d2
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-03-09 14:18:52 +0530

    Add stdlibs

--- src/EventStudies.jl
+++ src/EventStudies.jl
@@ -1,11 +1,15 @@
 module EventStudies
 
+using Dates, Statistics
+
 using TSFrames, DataFrames
 
 using HypothesisTests, Bootstrap # for inference
 
 using GLM # for models
 
+include("utils.jl")
+export remap_cumsum # remove before release
 include("models.jl")
 export NoModel, MarketModel, AugmentedMarketModel, ExcessReturn, ConstantMeanReturn
 include("inference.jl")
